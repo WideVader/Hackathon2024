c719d5ee4112d5a08918e0b852ad7e96dcb03f7b
dkremenovic <dkremenovic@outbrain.com>
2024-05-11 15:56:51 +0200

    folder structure

--- router.js
+++ router.js
@@ -2,7 +2,7 @@ const express = require('express');
 const router = express.Router();
 
 
-router.get('/example', (req, res) => {
+router.post('/transaction', (req, res) => {
   res.send('Example route');
 });
 
