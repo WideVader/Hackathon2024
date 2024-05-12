48fed23fb4064c3676211dcb0e78ad6659e3cc1e
UrhPercic <urh.percic12@gmail.com>
2024-05-12 09:26:26 +0200

    Passing data

--- demo_website/src/components/home/home.js
+++ demo_website/src/components/home/home.js
@@ -1,9 +1,63 @@
 import React, { useState } from "react";
 import "./home.css";
+import { addData } from "../../db/realTimeDatabase";
 
 function Home() {
-  // State to track the selected fraud type
   const [selectedFraudType, setSelectedFraudType] = useState(null);
+
+  const fraudulentData = {
+    user: {
+      id: "3",
+      name: "Alice Johnson",
+      location: "Madrid, Spain",
+      gender: "Female",
+      age: 95,
+      hobbies: ["Photography", "Cycling"],
+      currency: "EUR",
+      segment: "Senior",
+    },
+    transaction: {
+      id: "3",
+      location: "Paris, France",
+      user_agent: "Mozilla/5.0",
+      price: 1500,
+      currency: "EUR",
+      weather: "Sunny",
+      category: "Apparel",
+      issuer: "MasterCard",
+      product: "Luxury goods",
+      user_id: "3",
+      time: "2024-05-10T16:45:00Z",
+      fraud: null,
+    },
+  };
+
+  const nonFraudulentData = {
+    user: {
+      id: "3",
+      name: "Alice Johnson",
+      location: "Madrid, Spain",
+      gender: "Male",
+      age: 20,
+      hobbies: ["Photography", "Cycling"],
+      currency: "EUR",
+      segment: "Young Adult",
+    },
+    transaction: {
+      id: "3",
+      location: "Paris, France",
+      user_agent: "Mozilla/5.0",
+      price: 100,
+      currency: "USD",
+      weather: "Sunny",
+      category: "Electronics",
+      issuer: "MasterCard",
+      product: "Phone",
+      user_id: "3",
+      time: "2024-05-10T16:45:00Z",
+      fraud: null,
+    },
+  };
 
   const handleSelectCard = (isFraud) => {
     setSelectedFraudType(isFraud);
@@ -11,7 +65,9 @@ function Home() {
   };
 
   const handleSubmit = () => {
-    // You could add logic to send this data to a server here
+    const data = selectedFraudType ? fraudulentData : nonFraudulentData;
+    addData("users", data.user);
+    addData("transactions", data.transaction);
     console.log(
       `Submitted as ${selectedFraudType ? "fraudulent" : "not fraudulent"}`
     );
@@ -31,26 +87,32 @@ function Home() {
           onClick={() => handleSelectCard(false)}
         >
           <h2>Non-Fraudulent Transaction</h2>
-          <p>Location: New York, USA</p>
+          <hr></hr>
+          <p>Product: Phone</p>
           <p>User age: 20</p>
-          <p>Price: $100</p>
+          <p>Price: 100</p>
           <p>Currency: USD</p>
+          <p>Issuer: MasterCard</p>
+          <p>Gender: male</p>
           <p>Category: Electronics</p>
-          <p>Weather: Sunny</p>
+          <p>Segment: Young Adult</p>
         </div>
         <div
-          className={`fradulant card ${
+          className={`fraudulent card ${
             selectedFraudType === true ? "selected" : ""
           }`}
           onClick={() => handleSelectCard(true)}
         >
           <h2>Fraudulent Transaction</h2>
-          <p>Location: Paris, France</p>
+          <hr></hr>
+          <p>Product: Luxury goods</p>
           <p>User age: 95</p>
-          <p>Price: $1500</p>
+          <p>Price: 1500</p>
           <p>Currency: EUR</p>
+          <p>Issuer: MasterCard</p>
+          <p>Gender: male</p>
           <p>Category: Luxury Goods</p>
-          <p>Weather: Stormy</p>
+          <p>Segment: Senior</p>
         </div>
       </div>
       <div className="submit-button">
---
+++ demo_website/src/db/firebase.js
@@ -0,0 +1,17 @@
+import { initializeApp } from "firebase/app";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyCLpLOiDgRwcSkgklt0DlIuhXHj2uUOeZc",
+  authDomain: "hackaton-34125.firebaseapp.com",
+  databaseURL:
+    "https://hackaton-34125-default-rtdb.europe-west1.firebasedatabase.app",
+  projectId: "hackaton-34125",
+  storageBucket: "hackaton-34125.appspot.com",
+  messagingSenderId: "1096056460239",
+  appId: "1:1096056460239:web:3eecd1fb2eece4cc3638ba",
+  measurementId: "G-R2XRVHZSXN",
+};
+
+const app = initializeApp(firebaseConfig);
+
+export default app;
