4a5b2c514c8b47dafd7255c5026238a6997f9320
dkremenovic <dkremenovic@outbrain.com>
2024-05-12 09:29:45 +0200

    update interface schema, cronjob profile updates+train model, change initial detection and add weights to checks

--- services/detection_handler/index.js
+++ services/detection_handler/index.js
@@ -1,10 +1,19 @@
-const checks = require('./checks.js').default;
+
+const checks = require('./checks.js');
+//import predict
+import { predict } from '../logistical_regresion/index.js'
+
 
 
 export function validateRequest(company, transaction, user) {
     try {
-        transactionRateLimiter(company, transaction, user)
-        executeChecks(company, transaction, user)
+        if(transactionRateLimiter(company, transaction, user)){
+            return false
+        }
+        if (executeChecks(company, transaction, user) > 0.35) {
+            return predict(transaction)
+        }
+
         return true
     } catch (error) {
         return false
@@ -12,6 +21,7 @@ export function validateRequest(company, transaction, user) {
 }
 
 const transactionRateLimiter = (company, transaction, user) => {
+    //TODO: implement rate limiter for transactions
     if (company.transactionRateLimit > 10) {
         throw new Error('Transaction rate limit exceeded');
     }
@@ -22,10 +32,15 @@ const transactionRateLimiter = (company, transaction, user) => {
 
 const executeChecks = (company, transaction, user) => {
     try {
+        let totalWeight = 0
+        let checkWeights = []
         Object.keys(checks).forEach((key) => {
-            checks[key](company, transaction, user);
+            checkWeights.push(checks[key](company, transaction, user))
         });
+        checkWeights.forEach(weight => totalWeight += weight)
+        return totalWeight
     } catch (error) {
         console.log(error);
+        return 1
     }
 }
